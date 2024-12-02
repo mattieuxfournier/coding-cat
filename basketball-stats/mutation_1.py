53bbc955b0f3e657e42e38a270466968282a9f5d
mattieuxfournier <82413462+mattieuxfournier@users.noreply.github.com>
2024-12-02 13:25:44 -0500

    Update mutation_1.py

--- basketball-stats/mutation_1.py
+++ basketball-stats/mutation_1.py
@@ -1,9 +1,9 @@
 def bball_stats(twos: int, threes: int, shotsTaken: int):
   '''
-    
+    Wrong Sign
   '''
     import math 
     points = (twos * 2) + (threes * 3)
-    FG = str(math.floor(((twos + threes) / shotsTaken) * 100)) + "%"
+    FG = str(math.floor(((twos + threes) // shotsTaken)  + "%"
     return [points, FG]
 bball_stats
