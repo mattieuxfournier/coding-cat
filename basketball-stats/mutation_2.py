4930d350b9f9c133fb7f787097634fb8c12dbd61
mattieuxfournier <82413462+mattieuxfournier@users.noreply.github.com>
2024-12-02 13:28:25 -0500

    Create mutation_2.py

---
+++ basketball-stats/mutation_2.py
@@ -0,0 +1,9 @@
+def bball_stats(twos: int, threes: int, shotsTaken: int):
+  '''
+    Forgotten Brackets
+  '''
+    import math 
+    points = (twos * 2) + (threes * 3)
+    FG = str(math.floor(((twos + threes) / shotsTaken) * 100)) + "%"
+    return points, FG
+bball_stats
